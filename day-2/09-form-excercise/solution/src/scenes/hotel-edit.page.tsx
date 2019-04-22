5ae1d8635856b274bb5e384fdeca7e5be3888fe1
Braulio <braulio.diez@gmail.com>
2019-04-22 08:45:46 +0200

    testing setup on solution

---
+++ day-2/09-form-excercise/solution/src/scenes/hotel-edit.page.tsx
@@ -0,0 +1,10 @@
+import * as React from "react";
+import { Link } from "react-router-dom";
+import { routesLinks } from "core";
+import { AppLayout } from "layout";
+import { HotelCollectionContainer } from "pods/hotel-collection";
+
+export const HotelEditPage = () => (
+  <AppLayout>   
+  </AppLayout>
+);
