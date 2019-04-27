c26da9a5a13da4b367fdc00d0a76fc2302da0c76
Braulio Diez Botella <braulio.diez@gmail.com>
2019-04-27 21:38:24 +0200

    form filed firing as well

--- day-2/09-form-excercise/solution/src/pods/hotel-edit/hotel-edit.component.tsx
+++ day-2/09-form-excercise/solution/src/pods/hotel-edit/hotel-edit.component.tsx
@@ -1,13 +1,18 @@
-import * as React from 'react';
-import { createStyles, Theme, WithStyles, withStyles } from '@material-ui/core/styles';
-import { TextFieldForm, DropdownForm, TextAreaForm } from 'common/components';
-import { TextField, Button } from '@material-ui/core';
-import Rating from 'material-ui-rating';
-import { HotelEntityVm, HotelFormErrors } from './hotel-edit.vm';
-import { LookupEntity } from 'core';
-import { RatingForm } from 'common/components';
+import * as React from "react";
+import {
+  createStyles,
+  Theme,
+  WithStyles,
+  withStyles
+} from "@material-ui/core/styles";
+import { TextFieldForm, DropdownForm, TextAreaForm } from "common/components";
+import { TextField, Button } from "@material-ui/core";
+import Rating from "material-ui-rating";
+import { HotelEntityVm, HotelFormErrors } from "./hotel-edit.vm";
+import { LookupEntity } from "core";
+import { RatingForm } from "common/components";
 
-const styles = (theme : Theme) =>
+const styles = (theme: Theme) =>
   createStyles({
     formContainer: {
       display: "flex",
@@ -20,60 +25,60 @@ const styles = (theme : Theme) =>
   });
 
 interface Props extends WithStyles<typeof styles> {
-  hotel : HotelEntityVm;
-  cities : LookupEntity[];
-  onFieldUpdate : (id : string, value: any) => void;
-  formErrors : HotelFormErrors;
+  hotel: HotelEntityVm;
+  cities: LookupEntity[];
+  onFieldUpdate: (id: string, value: any) => void;
+  formErrors: HotelFormErrors;
+  onSave: () => void;
 }
-  
 
-export const HotelEditComponentInner = (props : Props) => {
-  const {classes, hotel, cities, onFieldUpdate, formErrors} = props;
+export const HotelEditComponentInner = (props: Props) => {
+  const { classes, hotel, cities, onFieldUpdate, formErrors, onSave } = props;
 
   return (
     <div className={classes.formContainer}>
-        <TextFieldForm
-          label="Name"
-          name="name"
-          value={hotel.name}
-          onChange={onFieldUpdate}
-          error={formErrors.name.errorMessage}
-        /> 
-        {/* for multilanguage support rather use formErrors.name.type */}
+      <TextFieldForm
+        label="Name"
+        name="name"
+        value={hotel.name}
+        onChange={onFieldUpdate}
+        error={formErrors.name.errorMessage}
+      />
+      {/* for multilanguage support rather use formErrors.name.type */}
 
-        <img className={classes.picture} src={hotel.picture}/>
+      <img className={classes.picture} src={hotel.picture} />
 
-        <RatingForm 
-          name="rating"
-          value={hotel.rating}
-          max={5}
-          onChange={onFieldUpdate}
-          error={formErrors.rating.errorMessage}
-        />
+      <RatingForm
+        name="rating"
+        value={hotel.rating}
+        max={5}
+        onChange={onFieldUpdate}
+        error={formErrors.rating.errorMessage}
+      />
 
-        <DropdownForm
-          name="city"
-          label="city"
-          onChange={onFieldUpdate}
-          value={hotel.city}
-          list={cities}
-          error={formErrors.city.errorMessage}
-        />
+      <DropdownForm
+        name="city"
+        label="city"
+        onChange={onFieldUpdate}
+        value={hotel.city}
+        list={cities}
+        error={formErrors.city.errorMessage}
+      />
 
+      <TextAreaForm
+        placeholder="Description"
+        name="description"
+        label="description"
+        value={hotel.description}
+        onChange={onFieldUpdate}
+        error={formErrors.description.errorMessage}
+      />
 
-        <TextAreaForm
-          placeholder="Description"
-          name="description"
-          label="description"
-          value={hotel.description}
-          onChange={onFieldUpdate}
-          error={formErrors.description.errorMessage}
-        />
-
-        <Button  variant="contained" color="primary">Save</Button>
-
+      <Button variant="contained" color="primary" onClick={onSave}>
+        Save
+      </Button>
     </div>
   );
-}
+};
 
 export const HotelEditComponent = withStyles(styles)(HotelEditComponentInner);
